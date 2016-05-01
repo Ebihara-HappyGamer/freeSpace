055ee7adf8572f379dd0d20b69757a41034271e2
Yuzuru Ebihara <info+ebihara@happygamer.co.jp>
2016-05-01 13:28:10 +0900

    最初のコミット

---
+++ Assets/ForButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnClick()
+    {
+        Debug.Log("クリックされたよ！！！");
+    }
+}
